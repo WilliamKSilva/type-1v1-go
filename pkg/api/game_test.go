b190a18d2d5053402bc3df0d918a307f29b96fb4
WilliamKSilva <wilz.kelvin12@gmail.com>
2023-02-09 15:31:01 -0300

    test: ensure NewGame game service method throws if no Player one name is provided

---
+++ pkg/api/game_test.go
@@ -0,0 +1,29 @@
+package api
+
+import (
+	"errors"
+	"testing"
+)
+
+
+type mockGameRepository struct {}
+
+func (m *mockGameRepository) Create (game *Game) error {
+    return nil
+}
+
+func TestShouldThrowIfPlayerOneNameIsMissing(t *testing.T) {
+    repo := &mockGameRepository{}
+    g := gameService{repo}
+
+    gameData := newGameData{
+        Timer: 120,
+    }
+
+    want := errors.New("Player one name is required")
+    _, err := g.NewGame(gameData)
+
+    if err.Error() != want.Error() {
+        t.Errorf("Expected: %s, got %s", want, err)
+    } 
+}
