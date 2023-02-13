d9b08a7519ee279da5969022708ebed9b4b8432a
william <wilz.kelvin12@gmail.com>
2023-02-13 18:59:07 -0300

    feat: add auto migrate for game table on db config

--- pkg/api/mysql/connect.go
+++ pkg/api/mysql/connect.go
@@ -3,14 +3,17 @@ package mysql
 import (
 	"fmt"
 
+	"github.com/WilliamKSilva/type-1v1/pkg/api"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func Connect() *gorm.DB {
-    dsn := "type1v1user:type1v1@tcp(127.0.0.1:3306)/type1v1?charset=utf8mb4&parseTime=True&loc=Local" 
+    dsn := "root:type1v1@tcp(127.0.0.1:3306)/type1v1?charset=utf8mb4&parseTime=True&loc=Local" 
     mysql := mysql.Open(dsn)
     gorm, err := gorm.Open(mysql, &gorm.Config{})
+
+    gorm.AutoMigrate(&api.Game{})
 
     if err != nil {
         panic(err)
