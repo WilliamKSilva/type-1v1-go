de160d281e3370618c54ca02a545cf655051157f
william <wilz.kelvin12@gmail.com>
2023-02-14 17:20:54 -0300

    test: ensure update Game method throw if no data is provided

--- pkg/api/game_test.go
+++ pkg/api/game_test.go
@@ -10,6 +10,7 @@ import (
 type mockGameRepository struct {
     mock.Mock
 }
+
 type mockTextService struct {
     mock.Mock
 }
@@ -20,6 +21,14 @@ func (m *mockGameRepository) Create (game *Game) error {
    game.ID = 1 
 
    return args.Error(1) 
+}
+
+func (m *mockGameRepository) Update (id uint, data UpdateGameData) (*Game, error) {
+   args := m.Called(id, data)
+
+   game := args.Get(0).(*Game)
+
+   return game, args.Error(1) 
 }
 
 func (m *mockTextService) GetRandomText (trigger string) (string, error) {
@@ -94,3 +103,22 @@ func TestShouldReturnAGameOnSuccess(t *testing.T) {
     } 
 }
 
+func TestUpdateShouldThrowIfNoDataIsProvided(t *testing.T) {
+    repo := new(mockGameRepository)
+    textService := new(mockTextService)
+
+    repo.On("Create", mock.Anything).Return(nil, nil)
+    textService.On("GetRandomText", mock.Anything).Return("bla bla bla bla", nil)
+
+    g := gameService{repo, textService}
+
+    id := uint(1) 
+    updateData := UpdateGameData{}
+
+    want := errors.New("Provide valid data to update an Game")
+    _, err := g.UpdateGame(id, updateData)
+
+    if err.Error() != want.Error() {
+        t.Errorf("Expected: %s, got %s", want.Error(), err.Error())
+    }
+}
