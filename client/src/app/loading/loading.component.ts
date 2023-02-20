3437a21e957acb95c1c1919382f62c5a23583234
william <wilz.kelvin12@gmail.com>
2023-02-20 18:00:45 -0300

    feat: add loading component

---
+++ client/src/app/loading/loading.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-loading',
+  templateUrl: './loading.component.html',
+  styleUrls: ['./loading.component.scss']
+})
+export class LoadingComponent {
+    @Input() isLoading: boolean = false
+
+    updateIsLoading (state: boolean): void {
+        this.isLoading = state
+    }
+}
